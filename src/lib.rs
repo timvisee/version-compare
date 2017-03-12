ad27a8c065b472f311c203aa2518ac933eeda592
timvisee <timvisee@gmail.com>
2017-03-12 22:24:05 +0100

    Create global version comparison method

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ mod comp_op;
 mod version;
 mod version_part;
 
+use comp_op::CompOp;
+use version::Version;
+
 /// Version compare structure.
 pub struct VersionCompare { }
 
@@ -11,5 +14,40 @@ impl VersionCompare {
     /// Create a new version compare instance.
     pub fn new() -> Self {
         VersionCompare { }
+    }
+
+    /// Compare two version number strings to each other.
+    /// This compares version `a` to version `b`, and returns whether version `a` is greater, less
+    /// or equal to version `b`.
+    ///
+    /// The two given version numbers must be valid, or an error will be returned.
+    ///
+    /// One of the following ok results may be returned:
+    /// - CompOp::EQ
+    /// - CompOp::LT
+    /// - CompOp::GT
+    ///
+    /// # Examples
+    ///
+    /// ```ignore
+    /// let version_compare = VersionCompare::new();
+    ///
+    /// // Compare version numbers
+    /// assert_eq!(version_compare.compare("1.2.3", "1.2.3"), Ok(CompOp::EQ));
+    /// assert_eq!(version_compare.compare("1.2.3", "1.2.4"), Ok(CompOp::LT));
+    /// assert_eq!(version_compare.compare("1", "0.1"), Ok(CompOp::GT));
+    /// ```
+    pub fn compare(a: &str, b: &str) -> Result<CompOp, ()> {
+        // Create version instances
+        let a_ver = Version::from(a);
+        let b_ver = Version::from(b);
+
+        // Both version numbers must have been parsed
+        if a_ver.is_none() || b_ver.is_none() {
+            return Err(());
+        }
+
+        // Compare and return the result
+        Ok(a_ver.unwrap().compare(&b_ver.unwrap()))
     }
 }
