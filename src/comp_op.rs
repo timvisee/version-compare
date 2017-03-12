e7624a8190069c7f8eb20252c4241950c044e99c
timvisee <timvisee@gmail.com>
2017-03-12 13:56:26 +0100

    Create inversion and opposite methods for comparison operator

--- src/comp_op.rs
+++ src/comp_op.rs
@@ -1,5 +1,11 @@
 /// Comparison operators.
 pub enum CompOp {
+    /// Equal to. (==)
+    EQ,
+
+    /// Not equal to. (!=)
+    NE,
+
     /// Less than. (<)
     LT,
 
@@ -12,9 +18,61 @@ pub enum CompOp {
     /// Greater than or equal to. (>=)
     GE,
 
-    /// Equal to. (==)
-    EQ,
+}
 
-    /// Not equal to. (!=)
-    NE
+impl CompOp {
+
+    /// Covert to the inverted comparison operator.
+    ///
+    /// This uses the following bidirectional rules:
+    /// - EQ <-> NE
+    /// - LT <-> GE
+    /// - LE <-> GT
+    pub fn as_inverted(self) -> Self {
+        self.invert()
+    }
+
+    /// Get the inverted comparison operator.
+    ///
+    /// This uses the following bidirectional rules:
+    /// - EQ <-> NE
+    /// - LT <-> GE
+    /// - LE <-> GT
+    pub fn invert(&self) -> Self {
+        match self {
+            &CompOp::EQ => CompOp::NE,
+            &CompOp::NE => CompOp::EQ,
+            &CompOp::LT => CompOp::GE,
+            &CompOp::LE => CompOp::GT,
+            &CompOp::GT => CompOp::LE,
+            &CompOp::GE => CompOp::LT
+        }
+    }
+
+    /// Convert to the opposite comparison operator.
+    ///
+    /// This uses the following bidirectional rules:
+    /// - EQ <-> NE
+    /// - LT <-> GT
+    /// - LE <-> GE
+    pub fn as_opposite(self) -> Self {
+        self.opposite()
+    }
+
+    /// Get the opposite comparison operator.
+    ///
+    /// This uses the following bidirectional rules:
+    /// - EQ <-> NE
+    /// - LT <-> GT
+    /// - LE <-> GE
+    pub fn opposite(&self) -> Self {
+        match self {
+            &CompOp::EQ => CompOp::NE,
+            &CompOp::NE => CompOp::EQ,
+            &CompOp::LT => CompOp::GT,
+            &CompOp::LE => CompOp::GE,
+            &CompOp::GT => CompOp::LT,
+            &CompOp::GE => CompOp::LE
+        }
+    }
 }
